74d1ff0c74cec53200683a4edf3a1ed5dc19af9b
Reeves Farrell <reevesfarrell13@gmail.com>
2025-02-18 12:52:52 -0500

    Completed exercise 2-2

---
+++ Chapter 2 - Pure Puzzles/2-2.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+
+using namespace std;
+
+/*
+Write a program that uses only three output statements,
+cout << "#", cout << " ", and cout << "\n"
+to produce a pattern of hash symbols shaped 
+as shown below.
+
+Example Output:
+
+   ##
+  ####
+ ######
+########
+########
+ ######
+  ####
+   ##
+
+*/
+
+void triangle() {
+
+    for (int row = 0; row < 4; row++) {
+
+        for (int col = 0; col < 3 - row; col++) {
+
+            cout << " ";
+        }
+
+        for (int col = 0; col < 2 + row * 2; col++) {
+
+            cout << "#";
+        }
+
+        cout << "\n";
+    }
+
+    for (int row = 0; row < 4; row++) {
+
+        for (int col = 0; col < 4 - abs(4 - row); col++) {
+
+            cout << " ";
+        }
+
+        for (int col = 0; col < 8 - row * 2; col++) {
+
+            cout << "#";
+        }
+
+        cout << "\n";
+    }
+}
+
+int main() {
+
+    triangle();
+
+    return 0;
+}
